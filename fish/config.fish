a0487b6919b373d8729d1755a130fc9aa7b87228
Dante Issaias <dante.issaias@gmail.com>
2019-04-16 12:12:09 +0300

    fixes #1
    Signed-off-by: Dante Issaias <dante.issaias@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,11 @@ set -x GPG_TTY (tty)
 #path
 set PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $PATH
 set PATH $FLUTTER_HOME/bin $PATH
+set PATH $HOME/.nvm/curre nt/bin $PATH
 
 alias rl="source ~/.config/fish/config.fish && echo '~/.config/fish/config.fish' reloaded"
+
+#source nvm
+bass source ~/.nvm/nvm.sh
 
 source ~/.config/fish/aliases.fish
