752d93fde56bf5219940fd57f8d1229d8dabf54a
Dante Issaias <dante.issaias@gmail.com>
2019-05-31 20:35:52 +0100

    🚀 Refactor fish configurations
    Signed-off-by: Dante Issaias <dante.issaias@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@
 set fish_greeting ""
 set fish_home ~/.config/fish
 
-# Source
+# Source other configuration files
 source $fish_home/environment.fish
 source $fish_home/abbrs.fish
+source $fish_home/local.fish
--- fish/environment.fish
+++ fish/environment.fish
@@ -1,21 +1,34 @@
+# Application Configuration
 set -gx EDITOR nvim
 set -gx PAGER less
 
-# Variables
-set -gx GPG_TTY (tty)
-set -gx FLUTTER_HOME $HOME/.flutterlib
+# Configuration Variables
+set -gx GPG_TTY (tty) # Fix Git GPG Signing
+set -gx LSCOLORS gxfxbEaEBxxEhEhBaDaCaD # Make ls colorful
+
+# Go Configuration
+set -gx GOPATH $HOME/go
+set PATH $GOPATH/bin $PATH
+
+# Android Configuration
 set -gx ANDROID_HOME $HOME/Library/Android/sdk
-set -gx LSCOLORS gxfxbEaEBxxEhEhBaDaCaD
-set -gx SPICETIFY_CONFIG $HOME/.config/spicetify
-
-
-# PATH
-set PATH $HOME/.local/bin $PATH
 set PATH $ANDROID_HOME/emulator $PATH
 set PATH $ANDROID_HOME/tools $PATH
-set PATH $ANDROID_HOME/tools/bin $PATH 
+set PATH $ANDROID_HOME/tools/bin $PATH
 set PATH $ANDROID_HOME/platform-tools $PATH
+
+# Flutter Configuration
+set -gx FLUTTER_HOME $HOME/.flutterlib
 set PATH $FLUTTER_HOME/bin $PATH
+
+# Spicetify Configuration
+set -gx SPICETIFY_CONFIG $HOME/.config/spicetify
+
+# Rust Configuration
 set PATH $HOME/.cargo/bin/ $PATH
+
+# Fastlane Configuration
 set PATH $HOME/.fastlane/bin $PATH
+
+# Mono Configuration
 set PATH /Library/Frameworks/Mono.framework/Versions/Current/bin/ $PATH
